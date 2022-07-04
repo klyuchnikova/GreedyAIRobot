e24a783fe56ab99aa2f2ce34697f5c4870ba6d8a
Ulyana Klyuchnikova <udklyuchnikova@edu.hse.ru>
2022-07-05 00:20:29 +0300

    game start!

---
+++ game_environment/__init__.py
@@ -0,0 +1,5 @@
+from gym.envs.registration import register
+
+register(id = 'BubbleShooter-v0',
+         entry_point='gym_bubbleshooter.envs:BubbleShooterEnv',
+         )
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name ='game_environment',
+    version = '0.1',
+    install_requires = ['gym', 'numpy', 'pygame']
+)
